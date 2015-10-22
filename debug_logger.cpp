4a5742924a1088e29a4c4677795f27189dca7dbc
Petr Uskov <petr.the.great960@gmail.com>
2015-10-23 03:16:48 +0800

    Solution

--- debug_logger.cpp
+++ debug_logger.cpp
@@ -1,10 +1,8 @@
 #include "debug_logger.h"
+#include <stdio.h>
+#include <fstream>
 
 using namespace std;
-
-namespace stxDebug {
-
-} // namespace stxDebug
 
 struct Data {
   Data(int num, std::wstring text) {
@@ -13,7 +11,53 @@ struct Data {
   }
   int num;
   std::wstring text;
+
 };
+
+namespace stxDebug {
+
+template<>
+struct GetDbgStringT<Data> {
+  static std::wstring GetDbgString(const Data& data) 
+  {
+    wchar_t sbuf[12];
+    std::wstring str = L"{ class: 'Data', object: { num: ";
+
+    swprintf(sbuf, 12, L"%d", data.num);
+    str += sbuf;
+    str += L" , text: " + data.text + L" } }";
+    
+    return str;
+  }
+};
+
+struct FileWriter
+{
+  void operator()(const std::wstring& msg) 
+  {
+    size_t dpos = msg.find(L'\n');
+    std::wstring fname_w = msg.substr(0, dpos);
+    std::string fname(fname_w.begin(), fname_w.end());
+
+    wofstream ofs(fname.c_str(), ios_base::app);
+
+    if (!ofs.bad()) {
+       ofs << msg.substr(dpos + 1) << std::endl; 
+    } 
+  }
+};
+
+class DebugFileLogger : public DebugLogger<FileWriter>
+{
+public:
+  DebugFileLogger(const std::wstring& fname) { *this << fname << L"\n"; }
+};
+
+stxDebug::DebugFileLogger File(const std::wstring& fname) {
+  return DebugFileLogger(fname);
+}
+
+} // namespace stxDebug
 
 int main(int argc, const char* argv[]) {
   cout << "stx C++ test task" << endl;
@@ -23,6 +67,8 @@ int main(int argc, const char* argv[]) {
 
   // stx: task is to uncomment and revive this line
   // stxDebug::File(L"dump.txt") << L"Dump of data: " << Data(55, "stx data");
+
+  stxDebug::File(L"dump.txt") << L"Dump of data: " << Data(55, L"stx data");
 
   return 0;
 }
